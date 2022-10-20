b1ec6321d851ffef777eaedeaee5f0b82bed310b
shafey281219965130 <shafey281219965130@gmail.com>
2022-10-21 00:02:32 +0200

    push

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "http://udegramx.eba-p5xxqfp3.us-east-1.elasticbeanstalk.com/",
 };
 
 /*
